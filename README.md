4d227946d6c5340fbcc9de5601352cdb878a0271
paarth-sharma <paarthsharma2003@gmail.com>
2024-03-17 19:01:50 +0000

    made a more refined implementation of multi-connection server and clients using the 'selector' library to multiplex the connections

--- README.md
+++ README.md
@@ -7,7 +7,36 @@
 - **Agreement** -  shared agreement and consensus on the collaborative activity, service or task to be provided.
 - **Action** - an action that demonstrates the meaningful and effective use of the agreed consensus which you have just achieved.
 
-### Bonus
+## Bonus
 Bonus acvity: Build into your consensus mechanism 
 1. A capability to detect malicious behaviour intended to meaningfully compromise or adversely impact the consensus approach, and 
 2. Effective mechanisms to anticipate and mitigate such approaches.
+
+
+## Modules and concepts used
+
+- [Socket](https://docs.python.org/3/library/socket.html)
+Something interesting to observe was - Blocking Calls
+
+A socket function or method that temporarily suspends your application is a blocking call. For example, .accept(), .connect(), .send(), and .recv() block, meaning they don’t return immediately. Blocking calls have to wait on system calls (I/O) to complete before they can return a value. So you, the caller, are blocked until they’re done or a timeout or other error occurs.
+
+Blocking socket calls can be set to non-blocking mode so they return immediately. If you do this, then you’ll need to at least refactor or redesign your application to handle the socket operation when it’s ready.
+
+Because the call returns immediately, data may not be ready. The callee is waiting on the network and hasn’t had time to complete its work. If this is the case, then the current status is the errno value socket.EWOULDBLOCK. Non-blocking mode is supported with .setblocking().
+
+**By default, sockets are always created in blocking mode.**
+
+### This meant that in the server, calls made to this socket will no longer block. 
+### When it’s used with sel.select(), you can wait for events on one or more sockets and then read and write data when it’s ready.
+
+- [Selector](https://docs.python.org/3/library/selectors.html)
+
+## How to?
+
+- To run this project, open up one cmd instance as administrator and run the ```server.py``` file with the 
+```python server.py <host> <port>``` command entering you host address and port number for communication.
+- Open another cmd instance as administrator and run the ```server.py``` file with the 
+```python server.py <host> <port> <no. of connections>``` command.
+
+### All non-privileged ports have value > 1023
+### Use host as 127.0.0.1 i.e., standard loopback interface address (localhost) for convenience or run the command ```ipconfig``` to find you local public ip on an administrative cmd instance. 
--- server.py
+++ server.py
@@ -1,18 +1,74 @@
-# Simple server that listens on port 65432 and echos back whatever it receives
-
+import sys
 import socket
+import selectors
+import types
 
-HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+# a selector object to multiplex input and output channels
+sel = selectors.DefaultSelector()
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+# HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+# PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+#instead of hardcoding the host and port, we will use command line arguments in a tuple
+host, port = sys.argv[1], int(sys.argv[2])
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((host, port))
+lsock.listen()
+print(f"Listening on {(host, port)}")
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+# call sock.accept() and then call conn.setblocking(False) to put the socket in non-blocking mode.
+# the main objective in this version of the server because you don’t want it to block. 
+# If it blocks, then the entire server is stalled until it returns. 
+# That means other sockets are left waiting even though the server isn't actively working.
+# If this isn't prevented, the server will "hang"
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+# 'key' is the 'namedtuple' returned from .select() that contains the socket object (fileobj) and data object. 
+# 'mask' contains the events that are ready.
+# if the socket is ready for reading, then mask & selectors.EVENT_READ will evaluate to True, so sock.recv() is called. 
+# any data that’s read is appended to data.outb so that it can be sent later.
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+
+        # If no data is received, this means that the client has closed their socket, so the server should too. 
+        # call sel.unregister() before closing, so it’s no longer monitored by .select().
+        else:
+            print(f"Closing connection to {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+
+    # .send() method returns the number of bytes sent. 
+    # we can slice .outb buffer to discard the bytes sent and only keep the unsent data.        
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
+# 
+try:
+    while True:
+        events = sel.select(timeout=None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
